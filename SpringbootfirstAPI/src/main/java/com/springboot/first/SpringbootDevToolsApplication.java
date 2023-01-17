040dab03599954ba482269fd22993c2986ba439b
anandpal-adt <anandpalsingh.adt@gmail.com>
2023-01-17 17:35:52 +0530

    commited new project of spring boot api

---
+++ SpringbootfirstAPI/src/main/java/com/springboot/first/SpringbootDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevToolsApplication.class, args);
+	}
+
+}
---
+++ SpringbootfirstAPI/src/main/java/com/springboot/first/services/BookService.java
@@ -0,0 +1,58 @@
+package com.springboot.first.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.springboot.first.entity.Book;
+
+@Component
+public class BookService {
+
+	
+	private static List<Book> list=new ArrayList<>();
+	
+	static {
+//		list.add(new Book( 12,"Let us C","Anand rajput"));
+//		list.add(new Book( 13,"Let us D","Anand rajput"));
+//		list.add(new Book( 14,"Let us E","Anand rajput"));
+//		list.add(new Book( 15,"Let us F","Anand rajput"));
+	}
+	
+	//getAll books
+	public List<Book> getAllBooks(){
+		return list;
+	}
+	
+	//get single book by id
+	public Book getBookById(int id) {
+		Book book=null;
+	book=list.stream().filter(o->o.getId()==id).findFirst().get();
+	return book;
+	}
+
+	//Adding book object
+	public Book addBook(Book book) {
+		list.add(book);
+		return book;
+	}
+	
+	//deleting book
+	public void deleteBook(int id) {
+		list=list.stream().filter(book->(book.getId()!=id)).collect(Collectors.toList());
+   //	list.remove(id);
+	}
+
+	//update book
+	public void updateBook(Book book, int bookId) {
+        list=list.stream().map(b->{
+        	if(b.getId()==bookId) {
+        		b.setAuthor(book.getAuthor());
+        		b.setTitle(book.getTitle());
+        	}
+        	return b;
+        	}).collect(Collectors.toList());
+	}
+}
